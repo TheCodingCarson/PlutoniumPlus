a0bf2ca2df71fe2fe73533fd1cae099db47ffd35
Coding Carson <coding@carson.rip>
2024-06-17 00:17:02 -0700

    Initial commit

---
+++ MainForm.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace PlutoniumPlus
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.textBoxPLInstallLocation = new System.Windows.Forms.TextBox();
+            this.plInstallDirectoryButton = new System.Windows.Forms.Button();
+            this.saveSettingsButton = new System.Windows.Forms.Button();
+            this.playButton = new System.Windows.Forms.Button();
+            this.buttonMovePLInstall = new System.Windows.Forms.Button();
+            this.buttonCreateShortcut = new System.Windows.Forms.Button();
+            this.checkBoxAutoClose = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // textBoxPLInstallLocation
+            // 
+            this.textBoxPLInstallLocation.Location = new System.Drawing.Point(12, 12);
+            this.textBoxPLInstallLocation.Name = "textBoxPLInstallLocation";
+            this.textBoxPLInstallLocation.Size = new System.Drawing.Size(209, 20);
+            this.textBoxPLInstallLocation.TabIndex = 0;
+            // 
+            // plInstallDirectoryButton
+            // 
+            this.plInstallDirectoryButton.Location = new System.Drawing.Point(12, 38);
+            this.plInstallDirectoryButton.Name = "plInstallDirectoryButton";
+            this.plInstallDirectoryButton.Size = new System.Drawing.Size(209, 23);
+            this.plInstallDirectoryButton.TabIndex = 1;
+            this.plInstallDirectoryButton.Text = "Select/Set Plutonium Client Directory";
+            this.plInstallDirectoryButton.UseVisualStyleBackColor = true;
+            this.plInstallDirectoryButton.Click += new System.EventHandler(this.plInstallDirectoryButton_Click);
+            // 
+            // saveSettingsButton
+            // 
+            this.saveSettingsButton.Location = new System.Drawing.Point(12, 160);
+            this.saveSettingsButton.Name = "saveSettingsButton";
+            this.saveSettingsButton.Size = new System.Drawing.Size(92, 23);
+            this.saveSettingsButton.TabIndex = 2;
+            this.saveSettingsButton.Text = "Save Settings";
+            this.saveSettingsButton.UseVisualStyleBackColor = true;
+            this.saveSettingsButton.Click += new System.EventHandler(this.saveSettingsButton_Click);
+            // 
+            // playButton
+            // 
+            this.playButton.Location = new System.Drawing.Point(123, 160);
+            this.playButton.Name = "playButton";
+            this.playButton.Size = new System.Drawing.Size(98, 23);
+            this.playButton.TabIndex = 3;
+            this.playButton.Text = "Play";
+            this.playButton.UseVisualStyleBackColor = true;
+            this.playButton.Click += new System.EventHandler(this.playButton_Click);
+            // 
+            // buttonMovePLInstall
+            // 
+            this.buttonMovePLInstall.Location = new System.Drawing.Point(12, 67);
+            this.buttonMovePLInstall.Name = "buttonMovePLInstall";
+            this.buttonMovePLInstall.Size = new System.Drawing.Size(209, 23);
+            this.buttonMovePLInstall.TabIndex = 4;
+            this.buttonMovePLInstall.Text = "Move Current Plutonium Client Install";
+            this.buttonMovePLInstall.UseVisualStyleBackColor = true;
+            this.buttonMovePLInstall.Click += new System.EventHandler(this.buttonMovePLInstall_Click);
+            // 
+            // buttonCreateShortcut
+            // 
+            this.buttonCreateShortcut.Location = new System.Drawing.Point(12, 131);
+            this.buttonCreateShortcut.Name = "buttonCreateShortcut";
+            this.buttonCreateShortcut.Size = new System.Drawing.Size(92, 23);
+            this.buttonCreateShortcut.TabIndex = 5;
+            this.buttonCreateShortcut.Text = "Create Shortcut";
+            this.buttonCreateShortcut.UseVisualStyleBackColor = true;
+            this.buttonCreateShortcut.Click += new System.EventHandler(this.buttonCreateShortcut_Click);
+            // 
+            // checkBoxAutoClose
+            // 
+            this.checkBoxAutoClose.AutoSize = true;
+            this.checkBoxAutoClose.Location = new System.Drawing.Point(12, 108);
+            this.checkBoxAutoClose.Name = "checkBoxAutoClose";
+            this.checkBoxAutoClose.Size = new System.Drawing.Size(131, 17);
+            this.checkBoxAutoClose.TabIndex = 6;
+            this.checkBoxAutoClose.Text = "Auto Close on Launch";
+            this.checkBoxAutoClose.UseVisualStyleBackColor = true;
+            this.checkBoxAutoClose.CheckedChanged += new System.EventHandler(this.checkBoxAutoClose_CheckedChanged);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(234, 195);
+            this.Controls.Add(this.checkBoxAutoClose);
+            this.Controls.Add(this.buttonCreateShortcut);
+            this.Controls.Add(this.buttonMovePLInstall);
+            this.Controls.Add(this.playButton);
+            this.Controls.Add(this.saveSettingsButton);
+            this.Controls.Add(this.plInstallDirectoryButton);
+            this.Controls.Add(this.textBoxPLInstallLocation);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.Text = "PlutoniumPlus";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxPLInstallLocation;
+        private System.Windows.Forms.Button plInstallDirectoryButton;
+        private System.Windows.Forms.Button saveSettingsButton;
+        private System.Windows.Forms.Button playButton;
+        private System.Windows.Forms.Button buttonMovePLInstall;
+        private System.Windows.Forms.Button buttonCreateShortcut;
+        private System.Windows.Forms.CheckBox checkBoxAutoClose;
+    }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Windows.Forms;
+using IWshRuntimeLibrary;
+using File = System.IO.File;
+
+namespace PlutoniumPlus
+{
+    public partial class MainForm : Form
+    {
+        private string configFilePath = "PlutoniumPlus.config";
+        private string defaultInstallDir = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "Plutonium");
+        private string selectedInstallDir;
+        private bool selectedAutoClose;
+
+        public MainForm(string[] args)
+        {
+            InitializeComponent();
+            LoadSettings();
+            UpdateMoveButtonVisibility();
+
+            if (args.Length > 0 && args[0] == "--SkipUI")
+            {
+                this.Hide();
+                RunPlutonium(true);
+            }
+        }
+
+        private void LoadSettings()
+        {
+            if (File.Exists(configFilePath))
+            {
+                var lines = File.ReadAllLines(configFilePath);
+                foreach (var line in lines)
+                {
+                    var parts = line.Split(new[] { '=' }, 2);
+                    if (parts.Length == 2)
+                    {
+                        var settingName = parts[0].Trim();
+                        var settingValue = parts[1].Trim().Trim('"');
+
+                        if (settingName == "InstallDir")
+                        {
+                            selectedInstallDir = settingValue;
+                        }
+                        else if (settingName == "AutoClose")
+                        {
+                            if (settingValue == "True")
+                            {
+                                selectedAutoClose = true;
+                                checkBoxAutoClose.Checked = true;
+                            }
+                            else
+                            {
+                                selectedAutoClose = false;
+                                checkBoxAutoClose.Checked = false;
+                            }
+                        }
+                    }
+                }
+            }
+            else
+            {
+                selectedInstallDir = defaultInstallDir;
+                SaveSettings();
+            }
+            textBoxPLInstallLocation.Text = selectedInstallDir;
+
+        }
+
+        private void SaveSettings()
+        {
+            using (StreamWriter writer = new StreamWriter(configFilePath))
+            {
+                writer.WriteLine($"InstallDir=\"{selectedInstallDir}\"");
+                writer.WriteLine($"AutoClose=\"{selectedAutoClose}\"");
+            }
+        }
+
+        private void RunPlutonium(bool AutoClose)
+        {
+            string plutoniumPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "plutonium.exe");
+            if (File.Exists(plutoniumPath))
+            {
+                Process.Start(plutoniumPath, $"-install-dir \"{selectedInstallDir}\"");
+
+                if (AutoClose)
+                {
+                    this.Close();
+                }
+            }
+            else
+            {
+                MessageBox.Show("plutonium.exe not found in the current directory.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void plInstallDirectoryButton_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog dialog = new OpenFileDialog())
+            {
+                dialog.ValidateNames = false;
+                dialog.CheckFileExists = false;
+                dialog.CheckPathExists = true;
+                dialog.FileName = "Select Folder";
+                dialog.InitialDirectory = AppDomain.CurrentDomain.BaseDirectory;
+
+                if (dialog.ShowDialog() == DialogResult.OK)
+                {
+                    selectedInstallDir = Path.GetDirectoryName(dialog.FileName);
+                    textBoxPLInstallLocation.Text = selectedInstallDir;
+                }
+            }
+        }
+
+        private void saveSettingsButton_Click(object sender, EventArgs e)
+        {
+            SaveSettings();
+            UpdateMoveButtonVisibility();
+            MessageBox.Show("Settings saved successfully.", "Info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void UpdateMoveButtonVisibility()
+        {
+            buttonMovePLInstall.Visible = Directory.Exists(defaultInstallDir) && selectedInstallDir != defaultInstallDir;
+        }
+
+        private void playButton_Click(object sender, EventArgs e)
+        {
+            RunPlutonium(selectedAutoClose);
+        }
+
+        private void buttonMovePLInstall_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (Directory.Exists(defaultInstallDir) && selectedInstallDir != defaultInstallDir)
+                {
+                    CopyDirectory(defaultInstallDir, selectedInstallDir);
+                    Directory.Delete(defaultInstallDir, true);
+                    MessageBox.Show("Plutonium installation moved successfully.", "Info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    buttonMovePLInstall.Visible = false;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error moving Plutonium installation: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void CopyDirectory(string sourceDir, string destDir)
+        {
+            Directory.CreateDirectory(destDir);
+
+            foreach (var file in Directory.GetFiles(sourceDir))
+            {
+                var destFile = Path.Combine(destDir, Path.GetFileName(file));
+                File.Copy(file, destFile, true);
+            }
+
+            foreach (var dir in Directory.GetDirectories(sourceDir))
+            {
+                var destSubDir = Path.Combine(destDir, Path.GetFileName(dir));
+                CopyDirectory(dir, destSubDir);
+            }
+        }
+
+        private void buttonCreateShortcut_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+                string shortcutName = "Plutonium Launcher";
+                string shortcutPath = Path.Combine(desktopPath, $"{shortcutName}.lnk");
+                string targetPath = Application.ExecutablePath;
+                string startInPath = AppDomain.CurrentDomain.BaseDirectory;
+
+                WshShell shell = new WshShell();
+                IWshShortcut shortcut = (IWshShortcut)shell.CreateShortcut(shortcutPath);
+                shortcut.Description = "Shortcut to Plutonium Launcher";
+                shortcut.TargetPath = targetPath;
+                shortcut.Arguments = "--SkipUI";
+                shortcut.WorkingDirectory = startInPath;
+                shortcut.Save();
+
+                MessageBox.Show("Shortcut created successfully on the desktop.", "Info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error creating shortcut: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void checkBoxAutoClose_CheckedChanged(object sender, EventArgs e)
+        {
+            selectedAutoClose = checkBoxAutoClose.Checked;
+            SaveSettings();
+        }
+    }
+}
